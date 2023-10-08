b9c85aa42d14d30940db8a2fd502f03fc8c63bff
Lucas <lucasilva589@gmail.com>
2023-10-08 13:41:24 -0300

    first commit

---
+++ die_e_help.py
@@ -0,0 +1,13 @@
+"""
+Utilitários Python para te auxiliar na programação
+
+dir -> Apresenta todos os atributos/propriedades e funções/metódos disponíveis
+para determinado tipo de dado ou variável
+
+dir(tipo de dado/variavel)
+
+help -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/metódos disponíveis
+para determinado tipo de dado ou variável
+
+help(tipo de dado/variavel.propriedade)
+"""
