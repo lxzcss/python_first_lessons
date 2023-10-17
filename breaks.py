05ff8c991e61cd974e7a3fecf5440c70127831e7
lucas <99770834+lxzcss@users.noreply.github.com>
2023-10-17 16:02:50 -0300

    First commit

---
+++ breaks.py
@@ -0,0 +1,25 @@
+"""
+Saindo de loops com break
+
+Funciona da mesma forma que nas linguagens C ou Java.
+
+Utilizamos o break para sair de loops de maneira projetada.
+"""
+
+# Exemplo 1
+
+for num in range(1, 11):
+    if num == 6:
+        break
+    else:
+        print(num)
+print('Saí do loop')
+
+
+# Exemplo 2
+
+while True:
+    cmd = input('Digite "sair" para sair: ')
+    if cmd == 'sair':
+        break
+print('Você saiu.')
---
+++ loop_while.py
@@ -0,0 +1,48 @@
+"""
+Loop while
+
+Forma geral
+
+while expressão_booleana:
+    //execução do loop
+
+O bloco do while será repetido enquanto a expressão booleana for verdadeira.
+
+Expressão Booleana é toda expressão onde o resultado é verdadeiro ou falso.
+
+# Exemplo 1
+
+num = 5
+
+num < 5
+
+# Exemplo 2
+
+num = 1
+
+while num < 10:
+    print(num)
+    # num = num + 1
+
+# Exemplo 3
+
+resp = ''
+
+while resp != 'sim':
+    resp = input('Já comeu hoje? ')
+
+# OBS: Em um loop while, é importante que cuidemos do criterio de parada para não causar um loop infinito
+
+# C ou Java
+
+while(expressão){
+    //execução
+}
+
+# do while (C ou Java)
+
+do {
+    //execução
+}while(expressão):
+
+"""
